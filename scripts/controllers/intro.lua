d08e6dd907f372045f4ca4ac5921643cf6e7f796
Ash Blue <ash@blueashes.com>
2013-03-10 00:32:26 -0800

    Completion of game intro and assets

--- scripts/controllers/intro.lua
+++ scripts/controllers/intro.lua
@@ -1,7 +1,7 @@
 local Intro = {}
 local dashbaord = require('scripts.controllers.dashboard')
 
-local DEBUG = true
+local DEBUG = false
 
 function Intro:new()
     o = display.newGroup()
@@ -19,12 +19,14 @@ function Intro:new()
     o:insert(background)
 
     -- Display Image text
-    local messageBackground = display.newRect(0, 0, display.contentWidth, display.contentHeight)
+    local messageBackground = display.newRect(0, 0, display.contentWidth + 400, display.contentHeight)
     messageBackground:setFillColor(0, 0, 0)
     messageBackground.alpha = 0
     o:insert(messageBackground)
 
-    local message = display.newRect(0, 0, 100, 100)
+    --local message = display.newRect(0, 0, 100, 100)
+    local message = display.newImage('resources/intro/text.png')
+
     message.alpha = 0
     message:setReferencePoint(display.CenterReferencePoint);
     message.x = display.contentWidth / 2
@@ -32,18 +34,24 @@ function Intro:new()
     o:insert(message)
 
     function o:showIntro()
-        messageBackground.alpha = 0.7
-        message.alpha = 1
-        timer.performWithDelay( 1000, o.destroy)
+        --messageBackground.alpha = 0.7
+        --message.alpha = 1
+        transition.to( messageBackground, { alpha = 0.7, time = 3000 } )
+        transition.to( message, { alpha = 1, time = 3000 } )
+        messageBackground:toFront()
+        message:toFront()
+        timer.performWithDelay( 12000, o.destroy)
     end
 
     -- Create ship offscreen and fly it into view until it shrinks and dissapears
-    local ship = display.newRect(0, 0, 200, 200)
+    local ship = display.newImageRect('resources/intro/spaceship.png', 700, 394)
     ship:setReferencePoint(display.TopCenterReferencePoint)
     ship.x = display.contentWidth / 2
     ship.y = display.contentHeight
-    transition.to( ship, { time = 5000, x = display.contentWidth / 2, y = 50, width = 0, height = 0, transition = easing.outExpo, onComplete = o.showIntro } )
+    transition.to( ship, { time = 20000, x = display.contentWidth / 2, y = -50, width = 0, height = 0, transition = easing.outExpo } )
     o:insert(ship)
+
+    timer.performWithDelay( 5000, o.showIntro )
 
     -- Destroy intro and cut to dashboard controller and turn controller
     function o:destroy()
