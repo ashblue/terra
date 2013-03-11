5202a7161412bae55c1d8bd92b1335497ded767d
Ash Blue <ash@blueashes.com>
2013-03-10 18:03:04 -0700

    Added droid icon files

--- main.lua
+++ main.lua
@@ -7,7 +7,7 @@
 display.setStatusBar(display.HiddenStatusBar)
 
 backgroundMusic = audio.loadStream( 'resources/intro/audio/music_bgm.mp3' )
-audio.play( backgroundMusic, { channel = 1, loops = -1, fadein = 20000 } )
+audio.play( backgroundMusic, { channel = 1, loops = -1, fadein = 10000, volume = 0.1 } )
 
 require('scripts.helpers.shuffle')
 local intro = require('scripts.controllers.intro').new()
